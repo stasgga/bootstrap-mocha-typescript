49686f7d4029cf5af44ff3b723e607d14c83e72d
stasgga <stsggrn25@gmail.com>
2020-07-08 14:22:22 +0200

    test update

--- test/client.ts
+++ test/client.ts
@@ -2,7 +2,7 @@ import { genAccount } from "../src/client";
 
 describe("package-info", () => {
     it("Should give the right package.json", () => {
-        const s = genAccount;
+        const s = genAccount();
         console.log("generated account:", s);
         if (s.length == 0) {
             throw Error("wrong account");
