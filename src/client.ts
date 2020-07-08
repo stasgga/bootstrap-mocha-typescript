2a69aa7800fd75ad59397868ba4cd771eb0e7b83
stasgga <stsggrn25@gmail.com>
2020-07-08 14:26:45 +0200

    adding a workaround in comments

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,7 @@
+// require works, but it disables type checks.
 // const algosdk = require("algosdk");
 
+// adding this is a workaround: @ts-ignore-line
 import * as algosdk from "algosdk";
 
 export function genAccount(): string {
